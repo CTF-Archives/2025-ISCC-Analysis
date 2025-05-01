8a1a52ef7e882fc3e28046bd96baae4d6c58e211
Randark <randark@foxmail.com>
2025-05-01 09:05:36 +0800

    init

---
+++ core/updateQuestion.py
@@ -0,0 +1,98 @@
+import requests
+import re
+import json
+import time
+from authorization import username, password
+
+host = "https://iscc.isclab.org.cn"
+
+
+ss = requests.Session()
+ss.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+
+ptnProfile = re.compile(r'<h1 id="team-id">(.*?)</h1>\s+<h3 class="text-center">总积分为:(\d*?),排在(\d*?)位。</h3>')
+
+
+def updateQuestionList(type, valueName):
+    ret = ss.get(f"{host}/{type}")
+    if ret.status_code == 200:
+        ret = ret.json()
+        for q in ret["game"]:
+            if q["category"] not in valueName.keys():
+                valueName[q["category"]] = {}
+            retret = ss.get(f'{host}/{type}/{q["id"]}')
+            if retret.status_code == 200:
+                retret = retret.json()
+                valueName[q["category"]][q["id"]] = {
+                    "name": retret["name"],
+                    "score": retret["value"],
+                    "description": retret["description"],
+                    "files": retret["files"],
+                    "solves": [],
+                }
+                if type == "arenas":
+                    valueName[q["category"]][q["id"]]["author"] = retret["author"]
+                # print(q["category"], q["id"], retret["value"], retret["name"], sep="\t")
+            else:
+                raise Exception(f"HTTP {retret.status_code}")
+            pass
+    else:
+        raise Exception(f"HTTP {ret.status_code}")
+    pass
+
+
+def updateQuestionsolve(type, valueName):
+    for category in valueName.keys():
+        for id in valueName[category].keys():
+            ret = ss.get(f"{host}/{type}/{id}/solves")
+            if ret.status_code == 200:
+                ret = ret.json()
+                valueName[category][id]["solves"] = ret["teams"]
+                if type == "are":
+                    for i in valueName[category][id]["solves"]:
+                        if i["name"] == valueName[category][id]["author"]:
+                            valueName[category][id]["solves"].remove(i)
+                            print(category, id, "成功排除擂台赛中出题人自动解题造成的误差")
+                            break
+                # print(category, id, valueName[category][id]["solves"], sep="\t")
+
+
+def updateQuestion():
+    challengeList = {}
+    arenaList = {}
+
+    # 检查登录情况
+    login_req =  ss.post(f"{host}/login", data={"name": username, "password": password}, allow_redirects=False).status_code == 302
+
+    print(login_req.to_bytes())
+    # 练武的题目信息
+    updateQuestionList("chals", challengeList)
+    # 练武的题目解出情况
+    updateQuestionsolve("chal", challengeList)
+    # 保存练武的信息
+    with open("./temp/challenge.json", "w", encoding="UTF-8") as f:
+        f.write(json.dumps(challengeList, ensure_ascii=False))
+
+    # 擂台的题目信息
+    updateQuestionList("arenas", arenaList)
+    # 擂台的题目解出情况
+    updateQuestionsolve("are", arenaList)
+    # 保存擂台的信息
+    with open("./temp/arena.json", "w", encoding="UTF-8") as f:
+        f.write(json.dumps(arenaList, ensure_ascii=False))
+
+    # 写入状态信息
+    with open("./temp/status.json", "w", encoding="UTF-8") as f:
+        res = requests.get("https://information.isclab.org.cn/get_totalcomp").json()
+        with open("./docs/data/user.json", "r", encoding="utf8") as f_:
+            user_data = json.load(f_)
+        f.write(
+            json.dumps(
+                {
+                    "updateTime": time.time(),
+                    "AllUserNumber": int(res["data"]),
+                    "ValidUserNumber": len(user_data.keys()),
+                },
+                ensure_ascii=False,
+            )
+        )
---
+++ main.py
@@ -0,0 +1,46 @@
+import datetime
+from pathlib import Path
+import json
+import shutil
+import os
+import core
+
+
+class ISCCError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.error_code = -1
+
+
+with open("./docs/data/status.json", "r") as f:
+    data_time = str(datetime.datetime.fromtimestamp(json.load(f)["updateTime"]).strftime("%Y-%m-%d_%H-%M-%S"))
+
+
+# 尝试获取新数据，如果失败就放弃本次爬取
+try:
+    core.updateQuestion()
+    core.generateUserList()
+except:
+    # TODO如果失败，将临时目录进行清理
+    raise ISCCError("ISCC 网站可用性降低，自动停止本次爬取")
+
+current_directory = os.getcwd()
+
+# 成功获取到数据，迁移旧数据
+destination_path = os.path.join(current_directory, "archives", data_time)
+Path(destination_path).mkdir(parents=True, exist_ok=True)
+source_folder = os.path.join(current_directory, "docs", "data")
+for root, directories, files in os.walk(source_folder):
+    for file in files:
+        source_file = os.path.join(root, file)
+        destination_file = os.path.join(destination_path, file)
+        shutil.move(source_file, destination_file)
+
+# 转移新数据
+destination_path = os.path.join(current_directory, "docs", "data")
+source_folder = os.path.join(current_directory, "temp")
+for root, directories, files in os.walk(source_folder):
+    for file in files:
+        source_file = os.path.join(root, file)
+        destination_file = os.path.join(destination_path, file)
+        shutil.move(source_file, destination_file)
